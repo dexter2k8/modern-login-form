07e98227217dc005fc01974331e4b73724ed6559
Sidnei <sidnei@tokenology.com>
2024-04-30 14:13:38 -0300

    created forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import "./App.css";
+import SocialCards from "./components/SocialCards";
 
 function App() {
   return (
     <>
-      <div className="Container">
+      <div className="container">
+        <form>
+          <h1>Create Account</h1>
+          <SocialCards />
+          <small>or use your email for registration</small>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+
         <form>
           <h1>Sign In</h1>
+          <SocialCards />
+          <small>or use your email password</small>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forget your password?</a>
+          <button>Sign In</button>
         </form>
       </div>
     </>
---
+++ src/components/SocialCards/index.tsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function SocialCards() {
+  return (
+    <div className="social-cards">
+      <a href="#">
+        <img src="src/assets/facebook-f.svg" alt="facebook" />
+      </a>
+      <a href="#">
+        <img src="src/assets/google-plus-g.svg" alt="facebook" />
+      </a>
+      <a href="#">
+        <img src="src/assets/github.svg" alt="facebook" />
+      </a>
+      <a href="#">
+        <img src="src/assets/linkedin-in.svg" alt="facebook" />
+      </a>
+    </div>
+  );
+}
