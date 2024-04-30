478f25efd244fce6aa92b666c023da1c816a47fd
Sidnei <sidnei@tokenology.com>
2024-04-30 11:40:44 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="Container">
+        <form>
+          <h1>Sign In</h1>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
