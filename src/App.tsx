87038c26cee4817435125e55fece93d945ec7039
Sidnei <sidnei@tokenology.com>
2024-04-30 15:44:22 -0300

    updated script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 import SocialCards from "./components/SocialCards";
 
 function App() {
+  const [isSignIn, setIsSignIn] = useState(true);
   return (
-    <>
-      <div className="container">
+    <div className={`container ${isSignIn ? "active" : ""}`}>
+      <div className="form-container sign-up">
         <form>
           <h1>Create Account</h1>
           <SocialCards />
@@ -14,7 +16,9 @@ function App() {
           <input type="password" placeholder="Password" />
           <button>Sign Up</button>
         </form>
+      </div>
 
+      <div className="form-container sign-in">
         <form>
           <h1>Sign In</h1>
           <SocialCards />
@@ -25,7 +29,24 @@ function App() {
           <button>Sign In</button>
         </form>
       </div>
-    </>
+
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all of site features</p>
+            <button onClick={() => setIsSignIn(false)}>Sign In</button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>
+              Register with your personal details to use all of site features
+            </p>
+            <button onClick={() => setIsSignIn(true)}>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
