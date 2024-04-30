5a28e7a798094fc17a6e018a7179209571325608
Sidnei <sidnei@tokenology.com>
2024-04-30 16:18:30 -0300

    updated image imports

--- src/components/SocialCards/index.tsx
+++ src/components/SocialCards/index.tsx
@@ -1,19 +1,23 @@
 import "./styles.css";
+import face from "../../assets/facebook-f.svg";
+import gplus from "../../assets/google-plus-g.svg";
+import github from "../../assets/github.svg";
+import linkedin from "../../assets/linkedin-in.svg";
 
 export default function SocialCards() {
   return (
     <div className="social-cards">
       <a href="#">
-        <img src="src/assets/facebook-f.svg" alt="facebook" />
+        <img src={face} alt="facebook" />
       </a>
       <a href="#">
-        <img src="src/assets/google-plus-g.svg" alt="facebook" />
+        <img src={gplus} alt="googleplus" />
       </a>
       <a href="#">
-        <img src="src/assets/github.svg" alt="facebook" />
+        <img src={github} alt="github" />
       </a>
       <a href="#">
-        <img src="src/assets/linkedin-in.svg" alt="facebook" />
+        <img src={linkedin} alt="linkedin" />
       </a>
     </div>
   );
